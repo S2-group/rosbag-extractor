3f0fc46303e95a7102a91917c2b4199f3d9d35c7
BerryC-VU <2672919@student.vu.nl>
2021-11-24 00:12:10 +0100

    Update ros.py; Added split.py

--- ros.py
+++ ros.py
@@ -1,26 +1,32 @@
 import rosbag
-import sys,os
+import sys
+import os
 import yaml
 import subprocess
-
-"""
-https://github.com/aktaylor08/RosbagPandas/blob/f9997baa9a681edaa670d27df78955a5941bb3a4/src/rosbag_pandas/rosbag_pandas.py#L208
-"""
+import string
 
 bagfile = sys.argv[1]
 print("filename: \n", bagfile)
 
-#get all info in the bag
-all_info = yaml.safe_load(subprocess.Popen(['rosbag', 'info', '--yaml', bagfile], stdout=subprocess.PIPE).communicate()[0])
-print("\ninfo: \n", all_info)
+# get all info in the bag
+all_info = yaml.safe_load(subprocess.Popen(
+    ['rosbag', 'info', '--yaml', bagfile], stdout=subprocess.PIPE).communicate()[0])
+#print("\ninfo: \n", all_info)
 
-#get all topic info in the bag
+# get all topic info in the bag
 topic_info = []
 topics = all_info['topics']
 for topic in topics:
     topic_info.append(topic['topic'])
 print("\ntopics: \n", topics)
 
-'''
-rqt_graph = yaml.safe_load(subprocess.Popen(['rqt_graph'], stdout=subprocess.PIPE).communicate()[0])
-'''
+
+time_start = all_info['start']
+time_end = all_info['end']
+time_duration = all_info['duration']
+print("\n start:    ", time_start, "\n  end:     ",
+      time_end, "\n duration: ", time_duration)
+
+
+yaml.safe_load(subprocess.Popen(['rqt_graph']).communicate()[0])
+
