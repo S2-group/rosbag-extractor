c3dad76fb52a681e025a94d79a09be88a69c10e8
BerryC-VU <2672919@student.vu.nl>
2022-03-16 19:10:26 +0100

    add publisher

--- git_api/run.sh
+++ git_api/run.sh
@@ -4,16 +4,18 @@
 
 ### extract repositories
 # extraction_script filename start_result_index terms username hash
-#./extract_repo.sh repos 0 rosbag BerryC-VU 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
+./extract_repo.sh repos 0 rosbag BerryC-VU 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
 
 ### extract commits
 # extraction_script filename start_result_index terms username hash
-#./extract_commits.sh commits 0 rosbag BerryC-VU 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
+./extract_commits.sh commits 0 rosbag BerryC-VU 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
 
 ### extract rosbag_experiment related repos+commits
-#./extract_ros_exp.sh ros_exp rosbag+experiment BerryC-VU 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
+./extract_ros_exp.sh ros_exp rosbag+experiment BerryC-VU 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
 
 ### merge all JSON files python3 ./merge.py json_path result_name
-#python3 ./merge.py data/repos merge/repos
+python3 ./merge.py data/repos merge/repos
 python3 ./merge.py data/commits merge/commits
-#python3 ./merge.py data/ros_exp merge/ros_exp
+python3 ./merge.py data/ros_exp merge/ros_exp
+
+
--- ros.py
+++ ros.py
@@ -9,24 +9,27 @@ bagfile = sys.argv[1]
 print("filename: \n", bagfile)
 
 # get all info in the bag
-all_info = yaml.safe_load(subprocess.Popen(
-    ['rosbag', 'info', '--yaml', bagfile], stdout=subprocess.PIPE).communicate()[0])
-#print("\ninfo: \n", all_info)
+all_info = yaml.safe_load(subprocess.Popen(['rosbag', 'info', '--yaml', bagfile], stdout=subprocess.PIPE).communicate()[0])
+# print("\nall info: \n", all_info)
 
 # get all topic info in the bag
 topic_info = []
 topics = all_info['topics']
 for topic in topics:
     topic_info.append(topic['topic'])
-print("\ntopics: \n", topics)
+# print("\ntopics: \n", topics)
 
+# publishers and subscribers
+# publishers = yaml.safe_load(subprocess.Popen(['rostopic', 'list', '-p', bagfile]).communicate()[0])
+# print("\npublishers: \n", publishers)
 
+'''
 time_start = all_info['start']
 time_end = all_info['end']
 time_duration = all_info['duration']
 print("\n start:    ", time_start, "\n  end:     ",
       time_end, "\n duration: ", time_duration)
+'''
 
-
-yaml.safe_load(subprocess.Popen(['rqt_graph']).communicate()[0])
-
+# generate computational graph
+yaml.safe_load(subprocess.Popen(['rqt_graph'], stdout=subprocess.PIPE).communicate()[0])
