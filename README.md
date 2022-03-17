66ea1b9de9ebb0a1c5acce40891e0a033f303f94
michelalbonico <bsd.albonico@gmail.com>
2022-03-17 00:09:48 -0300

    PyBag - Docker

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # rosbag_project
 
 Implementation of a tool for automatically extracting the computation graph of robotics systems out of mission logs. The project is based on ROS, the standard framework for implementing robotics software today. ROS provides also a standard format for logs collected at runtime, they are called bags. In this project you will develop a tool-based approach that, given as input a ROS bag, will automatically extract its software components (in terms of blocks, connections, and topics). In this project you will work on the ros bags available in this dataset about planetary exploration:  https://starslab.ca/enav-planetary-dataset/
+
+## Requirements
+Before running the graph extraction, you must install a basic ROS1 environment. Follow [this](http://wiki.ros.org/noetic/Installation/Ubuntu) tutorial.
+
+In Ubuntu, after setting the apt-get souce, run the following command:
+
+```
+sudo apt-get install ...
+```
---
+++ extract_graph.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 16 16:01:35 2022
+
+@author: michel
+"""
+
+import bagpy
+from bagpy import bagreader
+import pandas as pd
+
+b = bagreader('/home/michel/Desktop/SLAM_2021-06-03-12-32-22.bag')
+
+
+b.topic_table
+
+csvfiles = []
+for t in b.topics:
+    data = b.message_by_topic(t)
+    print(data)
+    csvfiles.append(data)
+#mt = b.message_types
+
+#nm = b.n_messages
+
+#bmessage = b.reader
+#all_info = yaml.safe_load(subprocess.Popen(bmessage, stdout=subprocess.PIPE).communicate()[0])
+
+#for bm in bmessage: 
+#    print(bm.message)
+#    all_info = yaml.safe_load(subprocess.Popen(bm.message, stdout=subprocess.PIPE).communicate()[0])
+
+    #print("######")
+    #print(bm.topic)
+    
+
+#yaml.safe_load(subprocess.Popen(['rqt_graph'], stdout=subprocess.PIPE).communicate()[0])
+
+
+#LASER_MSG = b.message_by_topic('/vehicle/front_laser_points')
+#LASER_MSG
+#df_laser = pd.read_csv(LASER_MSG)
+#df_laser
