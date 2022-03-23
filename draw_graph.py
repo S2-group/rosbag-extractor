339290e4ff9376c58d7e2dd77729b39d33b7f9ce
BerryC-VU <2672919@student.vu.nl>
2022-03-24 00:43:16 +0100

    draw graph update

--- draw_graph.py
+++ draw_graph.py
@@ -29,16 +29,16 @@ df = pd.read_csv(bagname + '/rosout.csv')
 df2 = pd.read_csv(bagname + '/rosout_agg.csv')
 df = pd.concat([df, df2]).reset_index()
 
-all_info = df[['name', 'msg', 'topics']]
+all_info = df[['name', 'topics']]
 # print(all_info)
 
 nodes = df['name'].unique()
 # print(nodes[1])
 
-graph_info = pd.DataFrame(data={'name': nodes}, columns=['name', 'msg', 'topics'])
+graph_info = pd.DataFrame(data={'name': nodes}, columns=['name', 'topics'])
 list_of_topics = []
-list_of_msgs = []
-substring = "Subscribing to "
+# list_of_msgs = []
+# substring = "Subscribing to "
 
 for i in range(len(nodes)):
     for j in range(len(all_info)):
@@ -46,11 +46,6 @@ for i in range(len(nodes)):
         if all_info['name'][j] == nodes[i]:
             # evaluate string as list and merge them into one list
             list_of_topics += ast.literal_eval(all_info['topics'][j])
-            # TODO: merge msgs;
-            #  -> ast_literal_eval is not working for '/'
-            # merge the message contains the substring "Subscribing to " with the same node name
-            # if substring in all_info['msg'][j]:
-            #    list_of_msgs += all_info['msg'][i][15:]
 
     # only keep the unique value in the list of topics
     # reference:
@@ -62,6 +57,9 @@ for i in range(len(nodes)):
 
 # DAG graph
 graph = nx.Graph()
+
+pos = nx.spring_layout(graph)  # positions for all nodes
+
 for i in range(len(nodes)):
     graph.add_node(nodes[i])
 
@@ -74,6 +72,10 @@ for i in range(len(nodes)):
         to_node = graph_info['topics'][i][j]
         graph.add_edge(from_node, to_node)
 
+# add fixed node and edges
+graph.add_node("/rosout")
+graph.add_edge("/rosout", "/rosout_agg")
+
 # print(graph.node())
 # print(graph.edges())
 
@@ -82,12 +84,11 @@ for i in range(len(nodes)):
 # node_color = "" Set node_color
 nx.is_directed(graph)
 nx.is_directed_acyclic_graph(graph)
-pos = nx.nx_agraph.graphviz_layout(graph)
-nx.draw_networkx(graph, pos, arrows=True)
+nx.draw_networkx(graph, arrows=True)
 
 # show graph
 plt.show()
 
 # save graph
 # plt.savefig(path, format="")
-plt.savefig("test.png", format="PNG")
+# plt.savefig("test.png", format="PNG")
