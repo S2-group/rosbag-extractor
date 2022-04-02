bbcbf240cc7e70c0d0c1f4645f40211f0531e464
BerryC-VU <2672919@student.vu.nl>
2022-04-02 21:20:17 +0200

    update extract_graph

--- extract_graph.py
+++ extract_graph.py
@@ -1,43 +1,103 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Mar 16 16:01:35 2022
+# python3 extract_graph.py bagfile
+# python3 extract_graph.py SLAM_2021-06-03-12-32-22.bag
+# python3 extract_graph.py turtlesim1.bag
 
-@author: michel
-"""
-
-import bagpy
 from bagpy import bagreader
 import pandas as pd
+import sys
+import ast
+from graphviz import Digraph
 
-b = bagreader('./turtlesim1.bag')
 
-print(b.topic_table)
+bagfile = sys.argv[1]
+bagname = bagfile.replace('.bag','')
+b = bagreader(bagfile)
 
+# topics into dataFrame for extraction to generate graph
+topics = pd.DataFrame(b.topics, columns=['Topics'])
+# print(topics)
+
+# list of csv files based on the topics
 csvfiles = []
 for t in b.topics:
     data = b.message_by_topic(t)
-    print(data)
+    # print(data)
     csvfiles.append(data)
-# mt = b.message_types
 
-# nm = b.n_messages
-
-# bmessage = b.reader
-# all_info = yaml.safe_load(subprocess.Popen(bmessage, stdout=subprocess.PIPE).communicate()[0])
-
-# for bm in bmessage:
-#    print(bm.message)
-#    all_info = yaml.safe_load(subprocess.Popen(bm.message, stdout=subprocess.PIPE).communicate()[0])
-
-    # print("######")
-    # print(bm.topic)
-    
-
-# yaml.safe_load(subprocess.Popen(['rqt_graph'], stdout=subprocess.PIPE).communicate()[0])
+# merge files and only keep needed columns
+df = pd.read_csv(bagname + '/rosout.csv')
+df2 = pd.read_csv(bagname + '/rosout_agg.csv')
+df = pd.concat([df, df2]).reset_index()
+all_info = df[['name', 'msg', 'topics']]
 
 
-# LASER_MSG = b.message_by_topic('/vehicle/front_laser_points')
-# LASER_MSG
-# df_laser = pd.read_csv(LASER_MSG)
-# df_laser
+graph = Digraph(name=bagname, strict=True)
+
+# nodes
+nodes = df['name'].unique()
+for node in nodes:
+    graph.node(node, node, {'shape': 'oval'})
+
+all_topics = topics['Topics'].values.tolist()
+sub_topics = []
+for i in range(len(all_topics)):
+    sub_topics += all_topics[i].split('/')[1:]
+
+for sub_topic in sub_topics:
+    if sub_topics.count(sub_topic) > 1 :
+        substring = '/' + sub_topic
+        # create clusters
+        with graph.subgraph(name='cluster_' + sub_topic) as sub_topic:
+            for topic in all_topics:
+                if substring in topic:
+                    sub_topic.node(topic, topic, {'shape': 'rectangle'})
+            sub_topic.attr(label=substring)
+
+# topics
+topics = topics['Topics'].values.tolist()
+for topic in topics:
+    if topic not in graph:
+        graph.node(topic, topic, {'shape': 'rectangle'})
+
+# merge subscribers for each node
+edge_info = pd.DataFrame(data={'name': nodes}, columns=['name', 'topics'])
+list_of_topics = []
+for i in range(len(nodes)):
+    for j in range(len(all_info)):
+        # merge topics with the same node name
+        if all_info['name'][j] == nodes[i]:
+            # evaluate string as list and merge them into one list
+            list_of_topics += ast.literal_eval(all_info['topics'][j])
+    # keep the unique value in the list of topics
+    edge_info['topics'][i] = list(set(list_of_topics))
+
+# edges
+for i in range(len(nodes)):
+    publisher = nodes[i]
+    for j in range(len(edge_info['topics'][i])):
+        subscriber = edge_info['topics'][i][j]
+        graph.edge(publisher, subscriber)
+
+# msg in both rosout and rosout_agg
+substring = "Subscribing to "
+for i in range(len(all_info['msg'])):
+    if substring in all_info['msg'].iloc[i]:
+        publisher = all_info['msg'].iloc[i].split(substring)[1]
+        subscriber = all_info['name'].iloc[i]
+        graph.edge(publisher, subscriber)
+
+# msg in rosout_agg
+# substring = "Subscribing to "
+# for i in range(len(df2['msg'])):
+#     if substring in df2['msg'].iloc[i]:
+#         publisher = df2['msg'].iloc[i].split(substring)[1]
+#         subscriber = df2['name'].iloc[i]
+#         graph.edge(publisher, subscriber)
+
+# add fixed node and edges
+graph.node("/fixed node", "/rosout", {'shape': 'oval'})
+graph.edge("/rosout", "/fixed node")
+graph.edge("/fixed node", "/rosout_agg")
+
+# generate graph
+graph.view()
