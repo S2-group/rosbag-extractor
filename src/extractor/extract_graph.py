3dbe1f1a367caf32ae41721756b9a7f6a873d8bb
BerryC-VU <2672919@student.vu.nl>
2022-06-02 12:59:18 +0200

    updata error msg checking

--- src/extractor/extract_graph.py
+++ src/extractor/extract_graph.py
@@ -5,98 +5,123 @@ import pandas as pd
 import sys
 import ast
 from graphviz import Digraph
-import os.path
+from rosbag import ROSBagException
 
 
-bagfile = sys.argv[1]
-bagname = bagfile.replace('.bag', '')
-b = bagreader(bagfile)
-
-# topics into dataFrame for extraction to generate graph
-topics = pd.DataFrame(b.topics, columns=['Topics'])
-# print(topics)
-
-# list of csv files based on the topics
-csvfiles = []
-topiclist = ['/rosout', '/rosout_agg']
-for t in topiclist:
-    data = b.message_by_topic(t)
+def read_rosout(b, bagname):
+    csvfiles = []
+    data = b.message_by_topic('/rosout')
     csvfiles.append(data)
 
-print("Finished extracting csv files")
+    rosout = pd.read_csv(bagname + '/rosout.csv')
+    all_info = rosout[['name', 'msg', 'topics']]
+    return all_info
 
-# # if no rosout file is present
-# if not os.path.exists(bagname + '/rosout.csv'):
-# merge files and only keep needed columns
-rosout = pd.read_csv(bagname + '/rosout.csv')
-rosout_agg = pd.read_csv(bagname + '/rosout_agg.csv')
-rosout = pd.concat([rosout, rosout_agg]).reset_index()
-all_info = rosout[['name', 'msg', 'topics']]
+def generate_topics(graph, all_topics):
+    sub_topics = []
+    for topic in all_topics:
+        sub_topics += topic.split('/')[1:]
+        if topic not in graph:
+            graph.node(topic, topic, {'shape': 'rectangle'})
+
+    for sub_topic in sub_topics:
+        if sub_topics.count(sub_topic) > 1:
+            substring = '/' + sub_topic
+            # create clusters
+            with graph.subgraph(name='cluster_'+ sub_topic) as sub_topic:
+                for topic in all_topics:
+                    if substring in topic:
+                        sub_topic.node(topic, topic, {'shape' : 'rectangle'})
+                sub_topic.attr (label=substring)
+
+def generate_edges(graph, all_info, nodes):
+    # merge subscribers for each node
+    edge_info = pd.DataFrame(data={'name': nodes}, columns=['name', 'topics'])
+    for i in range(len(nodes)):
+        list_of_topics = []
+        for j in range(len(all_info)):
+            # merge topics with the same node name
+            # print(all_info['name'][j])
+            if all_info['name'][j] == nodes[i]:
+                # evaluate string as list and merge them into one list
+                list_of_topics += ast.literal_eval(all_info['topics'][j])
+        # keep the unique value in the list of topics
+        edge_info['topics'][i] = list(set(list_of_topics))
+
+    # relationship contained in 'topics'
+    for i in range(len(nodes)):
+        publisher = nodes[i]
+        for j in range(len(edge_info['topics'][i])):
+            subscriber = edge_info['topics'][i][j]
+            graph.edge(publisher, subscriber)
+
+    # relationship contained in 'msg'
+    substring = "Subscribing to "
+    valid_msg = all_info['msg'].dropna()
+    for i in range(len(valid_msg)):
+        if substring in valid_msg.iloc[i]:
+            publisher = valid_msg.iloc[i].split(substring)[1]
+            subscriber = all_info['name'].iloc[i]
+            graph.edge(publisher, subscriber)
 
 
-graph = Digraph(name=bagname, strict=True)
+def extract_graph(bagname, topics, all_info):
+    graph = Digraph(name=bagname, strict=True)
 
-all_topics = topics['Topics'].values.tolist()
-sub_topics = []
-# topics
-topics = topics['Topics'].values.tolist()
-for topic in topics:
-    if topic not in graph:
-        graph.node(topic, topic, {'shape': 'rectangle'})
+    generate_topics(graph, topics)
 
-for i in range(len(all_topics)):
-    sub_topics += all_topics[i].split('/')[1:]
+    # if no '/rosout' topic in the bag file
+    if len(all_info) == 0:
+        # print("FALSE")
+        # nodes
+        graph.node("/rosbag_record", "/rosbag_record", {'shape': 'oval'})
+        graph.node("/rosout", "/rosout", {'shape': 'rectangle'})
 
-for sub_topic in sub_topics:
-    if sub_topics.count(sub_topic) > 1:
-        substring = '/' + sub_topic
-        # create clusters
-        with graph.subgraph(name='cluster_' + sub_topic) as sub_topic:
-            for topic in all_topics:
-                if substring in topic:
-                    sub_topic.node(topic, topic, {'shape': 'rectangle'})
-            sub_topic.attr(label=substring)
+        # edges
+        graph.edge("/rosbag_record", "/rosout")
+        for topic in topics:
+            graph.edge(topic, "/rosbag_record")
+    else:
+        # print("TRUE")
+        # nodes
+        nodes = all_info['name'].unique()
+        for node in nodes:
+            graph.node(node, node, {'shape': 'oval'})
+
+        # edges
+        generate_edges(graph, all_info, nodes)
+
+    # add fixed node and edges
+    graph.node("/fixed node", "/rosout", {'shape': 'oval'})
+    graph.edge("/rosout", "/fixed node")
+    graph.edge("/fixed node", "/rosout_agg")
+
+    # view graph
+    graph.unflatten(stagger=3, fanout=True).view()
 
 
-# nodes
-nodes = rosout['name'].unique()
-for node in nodes:
-    graph.node(node, node, {'shape': 'oval'})
+def main():
+    bagfile = sys.argv[1]
+    bagname = bagfile.replace('.bag', '')
 
-# merge subscribers for each node
-edge_info = pd.DataFrame(data={'name': nodes}, columns=['name', 'topics'])
-for i in range(len(nodes)):
-    list_of_topics = []
-    for j in range(len(all_info)):
-        # merge topics with the same node name
-        # print(all_info['name'][j])
-        if all_info['name'][j] == nodes[i]:
-            # evaluate string as list and merge them into one list
-            list_of_topics += ast.literal_eval(all_info['topics'][j])
-    # keep the unique value in the list of topics
-    edge_info['topics'][i] = list(set(list_of_topics))
+    while(True):
+        try:
+            b = bagreader(bagfile)
+            break
+        except ROSBagException as err:
+            print(err)
+            sys.exit()
 
-# edges
-for i in range(len(nodes)):
-    publisher = nodes[i]
-    for j in range(len(edge_info['topics'][i])):
-        subscriber = edge_info['topics'][i][j]
-        graph.edge(publisher, subscriber)
+    if '/rosout' in b.topics:
+        # print("TRUE")
+        all_info = read_rosout(b, bagname)
+    else:
+        # print("FALSE")
+        # print(b.topics)
+        sys.exit()
 
-# msg in both rosout and rosout_agg
-substring = "Subscribing to "
-valid_msg = all_info['msg'].dropna()
-for i in range(len(valid_msg)):
-    if substring in valid_msg.iloc[i]:
-        publisher = valid_msg.iloc[i].split(substring)[1]
-        subscriber = all_info['name'].iloc[i]
-        graph.edge(publisher, subscriber)
+    extract_graph(bagname, b.topics, all_info)
 
-# add fixed node and edges
-graph.node("/fixed node", "/rosout", {'shape': 'oval'})
-graph.edge("/rosout", "/fixed node")
-graph.edge("/fixed node", "/rosout_agg")
 
-# generate graph
-graph.unflatten(stagger=3,fanout=True).view()
-# graph.view()
+if __name__ == "__main__":
+    main()
