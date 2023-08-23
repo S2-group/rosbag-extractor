102dc69ee3d35d593d4847357711fdb4109fe50e
BerryC-VU <2672919@student.vu.nl>
2023-08-23 15:41:34 +0800

    Change time arguments in seconds

--- extractor.sh
+++ extractor.sh
@@ -5,17 +5,22 @@ ros_version=$1
 start_time=$2
 end_time=$3
 file=$4
+
 echo ">>>> Bag File Extractor <<<<"
 if [ -z "$2" ]; then
     echo "Use the following command: ./extractor.sh <ros_version> [<start_time>] [<end_time>] <bag_file>"
+#  python3
 else
     if [ -z "$4" ]; then
         if [ -z "$3" ]; then
             file=$2
+            start_time="start"
+            end_time="end"
         else
             file=$3
         fi
     fi
+
     case $ros_version in
         ros1)
             echo "Extracting ROS 1 Bag File"
@@ -23,9 +28,9 @@ else
             ;;
         ros2)
             echo "Extracting ROS 2 Bag File"
-            cd ./src/extractor/ros2
-            python3 ./main.py $file "${start_time}" "${end_time}"
-            cd -
+#            cd ./src/extractor/ros2
+            python3 ./src/extractor/ros2/main.py $file "${start_time}" "${end_time}"
+#            cd -
             ;;
         *)
             echo "ROS version is unknown"
